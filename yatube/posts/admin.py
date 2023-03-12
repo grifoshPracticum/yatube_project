82fb147d899c65974c376c2bbba1dd5ca23ce08b
Grigory <alihanov@gmail.com>
2023-03-12 17:27:05 +0300

    Комментарий

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Post, Group
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'text', 'pub_date', 'author', 'group', 'active')
+    empty_value_display = '-пусто-'
+    search_fields = ('text',)
+    list_filter = ('pub_date',)
+
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'active')
+    search_fields = ('title', 'description')
+    prepopulated_fields = {"slug": ("title",)}
--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,3 +1,58 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Group(models.Model):
+    title = models.CharField(
+        max_length=200,
+        verbose_name='Название группы',
+    )
+    slug = models.SlugField(
+        unique=True,
+        max_length=20,
+        verbose_name='Часть URL адреса группы',
+    )
+    description = models.TextField(
+        verbose_name='Описание группы',
+        help_text='Напишите о чем эта группа, какие посты в ней будут',
+    )
+    active = models.BooleanField(
+        default=True,
+        verbose_name='Видимость группы на сайте',
+    )
+    def __str__(self):
+        return self.title
+
+
+class Post(models.Model):
+    title = models.CharField(
+        max_length=200,
+        verbose_name='Название поста',
+        help_text='Назовите пост',
+        null=False,
+    )
+    text = models.TextField(
+        verbose_name='Текст вашего поста',
+    )
+    pub_date = models.DateTimeField(
+        verbose_name='Дата публикации',
+        auto_now_add=True,
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Автор поста',
+        null=False,
+        related_name='posts',
+    )
+    group = models.ForeignKey(
+        Group,
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='posts',
+        verbose_name='Группа для поста',
+        help_text='Выберите группу или оставьте пустым',
+    )
